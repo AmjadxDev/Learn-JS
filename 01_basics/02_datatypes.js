f6e67f3af79ad02594622bfedb64ff5c310448df
AmjadxDev <AmjadxDev@gmail.com>
2025-02-11 15:22:06 +0500

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // with this your code treat like new version of JS
+
+// alert("hello") // we cannot use this like in node we use this in browser
+
+let name = "Amjad"; // string
+let age = 27; // number  
+let isStudent = false; // boolean
+
+
+//   Premitive Datatypes
+
+// bigInt
+// null -> standalone value  -> eg let temp = null -> we cannot set 0
+// undefined -> for eg -> let state;
+// symbol -> use in react -> use for uniqueness
+
+
+// Non-Premitive Datatypes
+
+// object
+// array
+// null
+// null
+
+console.log(typeof(null));  // print object
+console.log(typeof undefined);  // print undefined
