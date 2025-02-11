fadd742a0965c04feeeaa63418ea7ed5324112ab
AmjadxDev <AmjadxDev@gmail.com>
2025-02-11 17:01:30 +0500

    comparision with different values and datatypes

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -14,12 +14,51 @@ let valueInNumber = Number(score);
 let isLoggedIn = 1; // "" -> false, "hites" -> true, 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(isLoggedIn);    // 1
-console.log(typeof booleanIsLoggedIn);  // boolean
+// console.log(isLoggedIn);    // 1
+// console.log(typeof booleanIsLoggedIn);  // boolean
 
 
 let someNumber = 124;
 let convertNumberToString  = String(someNumber);
 
-console.log(convertNumberToString); //"124"
-console.log(typeof convertNumberToString) // string
+// console.log(convertNumberToString); //"124"
+// console.log(typeof convertNumberToString) // string
+
+// ************ Operations ****************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue); // -3
+
+// console.log(2+2); // 4
+// console.log(2/3); // 0.66666666
+// console.log(2%3); // 2
+
+let str1 = "Hello";
+let str2 = " Hitesh";
+
+let str3 = str1 + str2;
+
+// console.log(str3); // Hello Hitesh
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2) // 122
+// console.log("1" + (2 + 2)) // 14
+// console.log(1 + 2 + "2") // 32
+// console.log(3 + 4 *5 % 2) // 3 never use practically
+
+// console.log(true);  // true -> true, +true -> 1,  true+ -> Error
+// console.log(1+""); // 1 but not recommended
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 4; // not recommended
+
+let gameCounter = 100;
+console.log(gameCounter++);
+let gameCounter1 = 100
+console.log(++gameCounter1);
+
+
+//https://tc39.es/ecma262/#sec-type-conversion  -> Link for study Js
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+
+
+// console.log("2" > 1);  // true but don't do like this always compare same datatypes
+
+// console.log(null > 0); // false  comparision convert null into 0 > , < , <= , >= convert null into Zero 
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+// console.log(null <= 0); // true
+// console.log(null === 0); // false
+
+// console.log(undefined == 0); // false udefined are always false
+
+// strict check ===
+// console.log("2" === 2); // false === can check also datatype
+
+
+
