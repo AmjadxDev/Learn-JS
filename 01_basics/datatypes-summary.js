79cd066a7036224fcd7fdda0336e45fc14dd9441
AmjadxDev <AmjadxDev@gmail.com>
2025-02-11 18:50:02 +0500

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+
+// # Primitive datatypes
+// total seven -> String, Number , Boolean, null, undefined, Symbol, BigInt (use for scienctific value)
+
+// let id = Symbol(1);
+// let anotherId = Symbol(1);
+// console.log(id == anotherId); // false
+
+// const bigIntNumber = 74563241335263451344n
+
+
+
+// # Refrence Type or Non Primitive datatypes
+
+// Arrays []
+// Objects {}
+// Functions  function name() {}
+
+let heros = ["shaktiman", "iron man", "x man", "black widow"];
+let myObj = {
+    name : "Ahmed",
+    age : 0
+    }
+
+const myFunction = function () { console.log("this is function");}
+
+console.log(typeof heros) // object
+console.log(typeof myObj) // object
+console.log(typeof myFunction) // function
