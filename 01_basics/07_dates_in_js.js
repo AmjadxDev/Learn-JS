371aec6addae26d93189bd8e527bb612124a83d2
AmjadxDev <AmjadxDev@gmail.com>
2025-02-12 14:36:56 +0500

    Date and time complete in detail also 01_Basics Complete

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString()); 
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString()); // show date time
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// console.log(typeof myDate) // object
+
+// let myCreatedDate = new Date(2023, 1, 23);
+let myCreatedDate = new Date(2023, 1, 23, 5, 30 , 43);
+let myCreatedDate1 = new Date("2022-01-14");
+// console.log(myCreatedDate.toDateString()); // month start from 0 in JS
+
+// console.log(myCreatedDate.toLocaleString());
+
+let timeStamp = Date.now();
+console.log(timeStamp);
+
