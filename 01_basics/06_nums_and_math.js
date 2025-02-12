22506a763084124e7e1a617c4f252e4bcd34a49b
AmjadxDev <AmjadxDev@gmail.com>
2025-02-12 12:20:53 +0500

    Complete details of Number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+let no = 100
+
+// console.log(no.toString().length);
+
+// console.log(no.toFixed(2)); // 100.00 add to decimal points
+
+const amount = 23.295823;
+// console.log(amount.toPrecision(3)); // 2 -> 23, 3 -> 23.3, 4 -> 23.30, 123.8 - 3 -> 124
+
+
+let money = 10000000;
+
+// console.log(money.toLocaleString()); // 10,000,000 show in US Format
+// console.log(money.toLocaleString("en-IN")); // 1,00,00,000 show in India Format
+
+// ++++++++++ Maths +++++++++++++++
+
+// console.log(Math.PI);
+ let neg = -93
+// console.log(Math.abs(neg)); // negative to posive
+
+// console.log(Math.round(4.5)); // 5
+
+// console.log(Math.min(43,423423,1234,23,5,42));  // 5
+
+// console.log(Math.random());  // very important
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1))+ 10);
+
+
+console.log(Math.floor(Math.random() * 6 + 1));
+
+
+
+
+
+
