80648fb387cd1eb4195532244613d0f406920eea
AmjadxDev <AmjadxDev@gmail.com>
2025-02-12 11:22:48 +0500

    string complete details

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+// we can denote string with "" or ''
+
+const name = "Xohaan";
+const repoCount = 50;
+
+// console.log(name + repoCount + " valuee"); // old way of sentax
+// console.info(`my name is ${name} and my repos is ${repoCount} value`); // string interpolation
+
+// const gameName = new String("Xohaan");
+// console.log(gameName.endsWith("a")); // false
+// console.log(gameName[0]); // we can access also like this
+// console.log(gameName.__proto__.name);\
+
+
+// console.log(gameName.charAt(1)); // position of character 
+// console.log(gameName.indexOf('a')); // 3
+
+// console.log(gameName.slice(0,3));
+// const  emailId = "    abc@gmail.com         "
+// console.log(emailId.trim());
+
+// const url = "https://www.google.com/hitesh%20choudhary";
+// console.log(url.replace("%20", "-")); // https://www.google.com/hitesh-choudhary
+// console.log(url.includes("hi "));  // ture
+
+
+
+const gameName = "hello-world-from-javascripts"
+console.log(gameName.split("-"));
+
+
+
+
+
+
+
