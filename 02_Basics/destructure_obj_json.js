626e7a46fad9a4b5833f2145810e313c417c6460
AmjadxDev <AmjadxDev@gmail.com>
2025-02-13 10:26:01 +0500

    Json Talk

---
+++ 02_Basics/destructure_obj_json.js
@@ -0,0 +1,28 @@
+const course = {
+    id: 1,
+    courseName: "Programming", 
+    coursePrice: "free",
+    courseInstructor : "xohaan"
+}
+// console.log(course.courseName);
+// const {courseInstructor} = course
+const {courseInstructor : instructor} = course  // de-structuring
+// console.log(instructor);
+
+
+// Json - API
+
+// {
+//     "name" : "xohaan",
+//     "courseName" : "JavaScript",
+//     "price" : "Free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+
